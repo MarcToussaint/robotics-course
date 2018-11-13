b03453b4d78a98948ba6c7864e4827812a8e033c
yoojinoh <5yoojinoh@gmail.com>
2018-11-13 14:27:02 +0100

    2nd coding exercise

---
+++ cpp/02-kinematics/main.cpp
@@ -0,0 +1,170 @@
+#include <Kin/kin.h>
+
+void simpleArrayOperations(){
+
+  /********* SEE PAGE 2 OF THE /doc/doc.pdf  ***********/
+  
+  arr x = {.1, .2, .3};         //directly setting the array
+  cout <<"x = " <<x <<endl;
+
+  x += arr({2., 2., 2.});        //adding to an array
+  cout <<"x = " <<x <<endl;
+
+  x *= 1.;
+  cout <<"x = " <<x <<endl;
+
+  arr y = {-.3,-.2,-.1};
+  y.append(x);                 //appending a vector to a vector
+  cout <<"y = " <<y <<endl;
+
+  arr M(4, 3, { 1, 0, 0,                //some 4x3 matrix
+                0, 1, 0,
+                0, 0, 1,
+                1, 0, 0 });
+
+  cout <<"M =\n" <<M <<endl;
+
+  cout <<"transpose M =\n" <<~M <<endl;  //matrix transpose
+
+  cout <<"M*x = " <<M*x <<endl;          //matrix-vector product
+
+  cout <<"M^-1 =\n" <<inverse(M) <<endl; //matrix inverse
+
+  M.append(M);                 //appending a matrix to a matrix
+  cout <<"M =\n" <<M <<endl;
+}
+
+
+void watchAConfig(){
+  rai::KinematicWorld K("man.ors");
+  cout <<"joint dimensions = " <<K.getJointStateDimension() <<endl;
+
+  cout <<"initial posture (hit ENTER in the OpenGL window to continue!!)" <<endl;
+  K.watch(true);        //pause and watch initial posture
+
+  arr q;
+  K.getJointState(q);
+
+  q(0) += 0.1;                 //change the first entry of q-vector
+  K.getJointState(q);
+  K.watch(true);
+  
+  q = 0.;                      //set q-vector equal zero
+  K.getJointState(q);
+  K.watch(true);
+}
+
+void reach(){
+  rai::KinematicWorld K("man.ors");
+  arr q,W;
+  uint n = K.getJointStateDimension();
+  K.getJointState(q);
+  double w = rai::getParameter("w",1e-4);
+  W.setDiag(w,n);  //W is equal the Id_n matrix times scalar w
+
+  cout <<"initial posture (hit ENTER in the OpenGL window to continue!!)" <<endl;
+  K.watch(true);        //pause and watch initial posture
+
+  arr y_target,y,J;
+  for(uint i=0;i<10;i++){
+    //1st task:
+    y_target = {-0.2, -0.4, 1.1}; 
+    K.evalFeature(y, J, FS_position, {"handR"});  //"handR" is the name of the right hand ("handL" for the left hand)
+
+    //compute joint updates
+    q += inverse(~J*J + W)*~J*(y_target - y); 
+    //NOTATION: ~J is the transpose of J
+    
+    //sets joint angles AND computes all frames AND updates display
+    K.setJointState(q);
+
+    //optional: pause and watch OpenGL
+    K.watch(true);
+  }
+}
+
+void circle(){
+  rai::KinematicWorld K("man.ors");
+  arr q,W;
+  uint n = K.getJointStateDimension();
+  K.getJointState(q);
+  double w = rai::getParameter("w",1e-4);
+  W.setDiag(w,n);  //W is equal the Id_n matrix times scalar w
+
+  K.watch(true);        //pause and watch initial posture
+
+
+  arr y_target,y,J;
+  for(uint i=0;i<1000;i++){
+    y_target = {-0.2, -0.4, 1.1};
+    y_target += .2 * arr({cos((double)i/20), 0, sin((double)i/20)});
+    
+    K.evalFeature(y, J, FS_position, {"handR"});  //"handR" is the name of the right hand ("handL" for the left hand)
+
+    cout <<i <<" current eff pos = " <<y <<"  current error = " <<length(y_target-y) <<endl;;
+    q += inverse(~J*J + W)*~J*(y_target - y);
+    K.setJointState(q);
+    K.watch(true);
+
+  }
+}
+
+
+
+void multiTask(){
+  rai::KinematicWorld K("man.ors");
+  arr q,y_target,yVec_target,y,J,yVec,JVec,W,Phi,PhiJ;
+  uint n = K.getJointStateDimension();
+  K.getJointState(q);
+
+  W.setDiag(1.,n);
+
+  K.watch(true);        //pause and watch initial posture
+
+
+  for(uint i=0;i<10000;i++){
+    K.getJointState(q);
+
+    Phi.clear();PhiJ.clear();
+
+    y_target = {-0.2, -0.4, 1.1};
+    y_target += .2 * arr({cos((double)i/20), 0, sin((double)i/20)});
+
+    //track circle with right hand
+    K.evalFeature(y, J, FS_position, {"handR"});  //"handR" is the name of the right hand ("handL" for the left hand)
+    Phi.append((y - y_target)/1e-2);
+    PhiJ.append( J / 1e-2 );
+
+    // 1st task: joint should stay close to zero
+
+
+
+    // 2nd task: left hand should point upwards
+
+
+
+    // 3rd task: robot should look at right hand
+
+
+
+    //compute joint updates
+    q -= 0.1*inverse(~PhiJ*PhiJ + W)*~PhiJ* Phi;
+    K.setJointState(q);
+    K.watch(true);
+
+  }
+}
+
+int main(int argc,char **argv){
+  rai::initCmdLine(argc,argv);
+
+  switch(rai::getParameter<int>("mode",4)){
+  case 0:  simpleArrayOperations();  break;
+  case 1:  watchAConfig();  break;
+  case 2:  reach();  break;
+  case 3:  circle();  break;
+  case 4:  multiTask();  break;
+  }
+
+  return 0;
+}
