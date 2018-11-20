2b9cf575c7a349fcc6f50836319fb928373317b6
yoojinoh <5yoojinoh@gmail.com>
2018-11-20 17:48:26 +0100

    coding exercise #03

--- cpp/03-dynamics/main.cpp
+++ cpp/03-dynamics/main.cpp
@@ -1,40 +1,56 @@
 #include <Kin/kin.h>
 
-void holdSteady(){
+void execTrajectory(){
   rai::KinematicWorld K("pegArm.ors");
   K.sortFrames();
-  double noise = 1.;
+  double noise = .01;
   bool gravity = true;
   uint n=K.getJointStateDimension();
 
   double tau=.01;
-  uint T=200;
-  
-  arr q,qdot;
+  uint N = 500;
+  double T = N*tau;
+
+  arr q,qdot, qddot_desired;
   arr M,F,u(n);
-  
+  arr integral = zeros(n);
+  arr q0, q_goal;
+
   K.getJointState(q, qdot);
+  q = q+0.4;
+  K.setJointState(q,qdot);
+
+  q_goal = zeros(n);
+  q0 = q;
+
   cout <<"initial posture (hit ENTER in the OpenGL window to continue!!)" <<endl;
   K.watch();
   
   ofstream dat("z.q");
 
-  for(uint t=0;t<=T;t++){
+  for(uint i=0;i<=N;i++){
+    double t = i * tau;
+    u = 0.;
+
+    // b) compute desired position, velocity and acceleration with sine motion profile
+
+
+    
+    // c) PD controller
+
+
+    // d) PID controller
+
+
+    // e) Inverse dynamics feedforward control
     K.equationOfMotion(M,F);
 
-    //no controller torques
-    u = 0.; //use M, F, and some desired qddot to compute u
+
 
     //dynamic simulation (simple Euler integration of the system dynamics, look into the code)
     K.stepDynamics(u, tau, noise, gravity);
     K.watch(false);
     K.getJointState(q, qdot);
-
-    //some impuls
-    if(t==100){
-      qdot+=1.;
-      K.setJointState(q, qdot);
-    }
 
     cout  <<" t=" <<tau*t  <<"sec E=" <<K.getEnergy()  <<"  q = " <<q <<endl;
     dat <<q <<endl;
@@ -47,7 +63,7 @@ void holdSteady(){
 
 int main(int argc,char **argv){
 
-  holdSteady();
+  execTrajectory();
   
   return 0;
 }
