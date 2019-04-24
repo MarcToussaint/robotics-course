8d1184589387d39ceff44119a9c1be45b179a83c
Marc Toussaint <marc.toussaint@informatik.uni-stuttgart.de>
2019-04-24 11:09:11 +0200

    webcam example in cpp

--- cpp/p2-cameraMini/main.cpp
+++ cpp/p2-cameraMini/main.cpp
@@ -1,19 +1,24 @@
 #include <Perception/opencv.h> //always include this first!
+#include <Perception/opencvCamera.h>
 #include <Perception/depth2PointCloud.h>
 #include <RosCom/roscom.h>
 #include <Kin/frame.h>
 
 void minimal_use(){
-  RosCom ROS;
 
   Var<byteA> rgb;
   Var<floatA> depth;
 
+#if 0 //using ros
+  RosCom ROS;
   SubscriberConv<sensor_msgs::Image, byteA, &conv_image2byteA> subRgb(rgb, "/camera/rgb/image_rect_color");
 //  SubscriberConv<sensor_msgs::Image, floatA, &conv_imageu162floatA> subDepth(depth, "/camera/depth_registered/image_raw");
+#else //using a webcam
+  OpencvCamera cam(rgb);
+#endif
 
   //looping images through opencv
-  for(uint i=0;i<20;i++){
+  for(uint i=0;i<100;i++){
     cv::Mat img = CV(rgb.get());
     if(img.total()>0){
       cv::imshow("RGB", img);
@@ -23,31 +28,31 @@ void minimal_use(){
   }
 }
 
-void get_objects_into_configuration(){
-  RosCom ROS;
+//void get_objects_into_configuration(){
+//  RosCom ROS;
 
-  Var<byteA> rgb;
-  Var<floatA> depth;
+//  Var<byteA> rgb;
+//  Var<floatA> depth;
 
-  SubscriberConv<sensor_msgs::Image, byteA, &conv_image2byteA> subRgb(rgb, "/camera/rgb/image_rect_color");
-  SubscriberConv<sensor_msgs::Image, floatA, &conv_imageu162floatA> subDepth(depth, "/camera/depth_registered/image_raw");
+//  SubscriberConv<sensor_msgs::Image, byteA, &conv_image2byteA> subRgb(rgb, "/camera/rgb/image_rect_color");
+//  SubscriberConv<sensor_msgs::Image, floatA, &conv_imageu162floatA> subDepth(depth, "/camera/depth_registered/image_raw");
 
 
-  Depth2PointCloud d2p(depth, 1.);
+//  Depth2PointCloud d2p(depth, 1.);
 
-  rai::KinematicWorld C;
-  C.addFile("model.g");
-  rai::Frame *pcl = C.addFrame("pcl", "camera", "shape:pointCloud");
-  for(uint i=0;i<100;i++){
-//    cout <<d2p.points.get()->N <<endl;
-    pcl->shape->mesh().V = d2p.points.get();
-    pcl->shape->mesh().V.reshape(640*480,3);
-    C.watch(false);
-    rai::wait(.1);
-  }
+//  rai::KinematicWorld C;
+//  C.addFile("model.g");
+//  rai::Frame *pcl = C.addFrame("pcl", "camera", "shape:pointCloud");
+//  for(uint i=0;i<100;i++){
+////    cout <<d2p.points.get()->N <<endl;
+//    pcl->shape->mesh().V = d2p.points.get();
+//    pcl->shape->mesh().V.reshape(640*480,3);
+//    C.watch(false);
+//    rai::wait(.1);
+//  }
 
-  rai::wait();
-}
+//  rai::wait();
+//}
 
 
 int main(int argc,char **argv){
