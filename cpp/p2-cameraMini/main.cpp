42d356c70f6284196338b39bd966b55ec65365ed
Marc Toussaint <marc.toussaint@informatik.uni-stuttgart.de>
2019-05-06 20:46:51 +0200

    baxter depth camera calib

--- cpp/p2-cameraMini/main.cpp
+++ cpp/p2-cameraMini/main.cpp
@@ -19,7 +19,7 @@ void minimal_use(){
 #endif
 
   //looping images through opencv
-  for(uint i=0;i<100;i++){
+  for(uint i=0;i<1000;i++){
     _rgb.waitForNextRevision();
     {
       cv::Mat rgb = CV(_rgb.get());
@@ -28,11 +28,13 @@ void minimal_use(){
       if(rgb.total()>0 && depth.total()>0){
         cv::imshow("rgb", rgb); //white=2meters
         cv::imshow("depth", 0.5*depth); //white=2meters
-        cv::waitKey(1);
+        int key = cv::waitKey(1);
+        if((key&0xff)=='q') break;
       }
     }
   }
 }
+
 
 void get_objects_into_configuration(){
   Var<byteA> _rgb;
@@ -40,26 +42,37 @@ void get_objects_into_configuration(){
 
   RosCamera cam(_rgb, _depth, "cameraRosNode", "/camera/rgb/image_rect_color", "/camera/depth_registered/image_raw");
 
-  Depth2PointCloud d2p(_depth, 600.f, 600.f, 320.f, 240.f);
+  double f = 1./tan(0.5*62.8*RAI_PI/180.);
+  f *= 320.;
+  arr Fxypxy = {f, f, 320., 240.}; //intrinsic camera parameters
+
+  Depth2PointCloud d2p(_depth, Fxypxy);
 
   BaxterInterface B(true);
 
   rai::KinematicWorld C;
   C.addFile("model.g");
-  C.setJointState(B.get_q());
 
   rai::Frame *pcl = C.addFrame("pcl", "camera");
+//  pcl->Q.setText("d(-90 0 0 1) t(-.05 .13 -.25) d(4 0 1 0) d(-67 1 0 0)");
+//  pcl->calc_X_from_parent();
+
   for(uint i=0;i<10000;i++){
     _rgb.waitForNextRevision();
+
+    arr q_real = B.get_q();
+    if(q_real.N==C.getJointStateDimension())
+      C.setJointState(q_real);
 
     if(d2p.points.get()->N>0){
       C.gl().dataLock.lock(RAI_HERE);
       pcl->setPointCloud(d2p.points.get());
       C.gl().dataLock.unlock();
-      C.watch(false);
+      int key = C.watch(false);
+      if(key=='q') break;
     }
 
-    {
+    { //display
       cv::Mat rgb = CV(_rgb.get());
       cv::Mat depth = CV(_depth.get());
 
@@ -69,17 +82,23 @@ void get_objects_into_configuration(){
         cv::waitKey(1);
       }
     }
+
+    //how to convert image to 3D coordinates:
+    double x_pixel_coordinate=0., y_pixel_coordinate=0., depth_from_depthcam=1.2;
+    arr pt = { x_pixel_coordinate, y_pixel_coordinate, depth_from_depthcam };
+    depthData2point(pt, Fxypxy); //transforms the point to camera xyz coordinates
+    pcl->X.applyOnPoint(pt); //transforms into world coordinates
   }
 
-  rai::wait();
+//  rai::wait();
 }
 
 
 int main(int argc,char **argv){
   rai::initCmdLine(argc,argv);
 
-  minimal_use();
-//    get_objects_into_configuration();
+//  minimal_use();
+    get_objects_into_configuration();
 
   return 0;
 }
--- cpp/p2-cameraMini/model.g
+++ cpp/p2-cameraMini/model.g
@@ -1,4 +1,8 @@
-Include = '../../rai-robotModels/baxter/baxter.g'
+Include = '../../rai-robotModels/baxter/baxter_new.g'
 
-frame camera (head) { shape:marker, Q:<d(-90 0 0 1) t(0 .1 -.2) d(-75 1 0 0)>, color:[.8 .8 0] size:[.2], focalLength:0.895, width:640, height:480, zRange:[.1 10] }
+frame camera (head) { shape:marker,
+#                      Q:<d(-90 0 0 1) t(-.05 .13 -.25) d(4 0 1 0) d(-67 1 0 0)>,
+                      Q:<d(180 0 0 1) t(-.150 0.073 0.071) E(-2.598 -0.052 -1.485)>,
+                      color:[.8 .8 0] size:[.2], focalLength:0.895, width:640, height:480, zRange:[.1 10] }
+
 frame cameraBlock (camera) { shape:box size:[.2 .03 .06] color:[0 0 0] }
