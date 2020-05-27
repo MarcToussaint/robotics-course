f1aee6bcdb9e05771cf2cf73d79e60c309f14685
Marc Toussaint <toussaint@tu-berlin.de>
2020-05-27 17:52:06 +0200

    example for adding multiple cameras

--- course3-Simulation/04-opencv/main.cpp
+++ course3-Simulation/04-opencv/main.cpp
@@ -111,11 +111,55 @@ void use_within_simulation(){
 
 //===========================================================================
 
+void multipleCameras(){
+  rai::Configuration RealWorld;
+  RealWorld.addFile("../../scenarios/challenge.g");
+
+  //change the position of the central sensor
+  rai::Frame* f = RealWorld.getFrameByName("camera");
+  f->setPosition(f->getPosition() + arr{0.,0.,.5});
+
+  //add a frame for the additional camera
+  f = RealWorld.addFrame("R_gripperCamera", "R_gripper");
+  f->setRelativePosition({.0, .1, 0.});
+  f->setShape(rai::ST_marker, {.5});
+
+  rai::Simulation S(RealWorld, S._physx, true);
+  S.cameraview().addSensor("camera"); //camera is a pre-existing frame that specifies the intrinsic camera parameter
+  S.cameraview().addSensor("Rcamera", "R_gripperCamera", 640, 360, 1.); //R_gripperCamera is a fresh frame - we have to specify intrinsic parameters explicitly
+
+  rai::Configuration C;
+  C.addFile("../../scenarios/pandasTable.g");
+
+  rai::ConfigurationViewer V;
+  V.setConfiguration(C, "model world start state");
+
+  byteA _rgb;
+  floatA _depth;
+
+  for(uint k=0;k<5;k++){
+    //get images from the wrist
+    S.cameraview().selectSensor("Rcamera");
+    S.getImageAndDepth(_rgb, _depth);
+    rai::wait();
+
+    //get images from the main sensor
+    S.cameraview().selectSensor("camera");
+    S.getImageAndDepth(_rgb, _depth);
+    rai::wait();
+  }
+
+
+}
+
+//===========================================================================
+
 int main(int argc,char **argv){
   rai::initCmdLine(argc,argv);
 
 //  minimal_use_with_webcam();
-  use_within_simulation();
+//  use_within_simulation();
+  multipleCameras();
 
   return 0;
 }
